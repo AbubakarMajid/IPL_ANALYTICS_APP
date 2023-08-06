595f7fe744d995cc1a0143ad87e237cd617f4419
Abubakar Majid <101563048+AbubakarMajid@users.noreply.github.com>
2023-08-06 16:06:25 +0500

    update

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ if user_menu == 'Season Summary':
     
     st.markdown("<h1 style='text-align: center; font-family:georgia,garamond,serif;font-size:40px;'>Season Summary</h1>", unsafe_allow_html=True)
     
-    match_per_season = df1.season.value_counts().reset_index().rename({'index':'season','season':
-                                                                           'matches'},axis = 1).sort_values("season")
+    match_per_season = df1.season.value_counts().reset_index().rename({'season':'matches','index':'season'},axis = 1)
+    match_per_season = match_per_season.sort_values("season")
     
     season_score = season_data.groupby('season')['total_runs'].sum().reset_index()
 
